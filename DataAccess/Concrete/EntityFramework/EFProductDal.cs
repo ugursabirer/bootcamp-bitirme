7ae59ded421879d39d3842c4518364db961f62f9
ugursabirer <sabirerugur@gmail.com>
2022-05-31 22:01:25 +0300

    architected inmemory and tested on console

---
+++ DataAccess/Concrete/EntityFramework/EFProductDal.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EFProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return new List<Product> { 
+                new Product { ProductId = 6, CategoryId = 1, ProductName = "Kazak", UnitPrice = 60, UnitsInStock = 12 },
+                new Product { ProductId = 7, CategoryId = 2, ProductName = "Eşofman Altı", UnitPrice = 90, UnitsInStock = 15 }
+            };
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product { ProductId = 1, CategoryId = 1, ProductName = "Tişört", UnitPrice = 50, UnitsInStock = 10 },
+                new Product { ProductId = 2, CategoryId = 2, ProductName = "Şort", UnitPrice = 75, UnitsInStock = 15 },
+                new Product { ProductId = 3, CategoryId = 1, ProductName = "Gömlek", UnitPrice = 100, UnitsInStock = 20 },
+                new Product { ProductId = 4, CategoryId = 1, ProductName = "Yelek", UnitPrice = 125, UnitsInStock = 25 },
+                new Product { ProductId = 5, CategoryId = 2, ProductName = "Pantolon", UnitPrice = 150, UnitsInStock = 30 }
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.ProductName = product.ProductName; 
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
