a4e1b0358e4e77ec04620f241a6877244fc37afb
ugursabirer <sabirerugur@gmail.com>
2022-06-03 12:48:18 +0300

    utilities used, magic strings and constants messages improved

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Product Name Invalid";
+        public static string MaintenanceTime = "Maintenance Time";
+        public static string ProductsListed = "Products Listed";
+    }
+}
